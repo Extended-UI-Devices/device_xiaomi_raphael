3f4aa277e52d752d7a1eeb6e7c1867661ab56007
Alexandru Scurtu <sasha.scurtu@gmail.com>
2020-12-27 13:09:54 +0200

    raphael: remove-package: nuke via too

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CarrierSetup Drive MyVerizonServices OBDM_Permissions PrebuiltGmail Showcase SprintDM SprintHM Podcasts YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio
+LOCAL_OVERRIDES_PACKAGES := CarrierSetup Drive MyVerizonServices OBDM_Permissions PrebuiltGmail Showcase SprintDM SprintHM Podcasts Via YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
